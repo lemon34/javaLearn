3fbda5d606a1f7f3c74e71ce7b93ed2baf82b19f
fuhailong <fuhailong@meituan.com>
2022-05-30 10:09:37 +0800

    test: 增加nio的测试例子

--- java-learn/src/main/java/netty/UserBuffer.java
+++ java-learn/src/main/java/netty/UserBuffer.java
@@ -1,5 +1,6 @@
 package netty;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,11 +19,33 @@ public class UserBuffer  {
         log.info("limit:{}", intBuffer.limit());
         log.info("capacity:{}", intBuffer.capacity());
     }
+    
+    public static void putTest() {
+        for (int i = 0; i < 5; ++i) {
+            intBuffer.put(i);
+        }
+        log.info("---after put---");
+        log.info("position:{}", intBuffer.position());
+        log.info("limit:{}", intBuffer.limit());
+        log.info("capacity:{}", intBuffer.capacity());
+    }
+
+    public static void flipTest() {
+        intBuffer.flip();
+        log.info("---after flip---");
+        log.info("position:{}", intBuffer.position());
+        log.info("limit:{}", intBuffer.limit());
+        log.info("capacity:{}", intBuffer.capacity());
+    }
 
     public static void main(String[] args) {
         log.info("11212");
         UserBuffer userBuffer = new UserBuffer();
         UserBuffer.allocateTest();
+        UserBuffer.putTest();
+        UserBuffer.flipTest();
+        Gson gson = new Gson();
+        log.info("111{}", gson.toJson(UserBuffer.intBuffer));
     }
 
 }
