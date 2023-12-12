9be807fd6a55b361fee809748b8f943b8ce19f68
fuhailong <fuhailong@meituan.com>
2023-12-12 19:59:51 +0800

    feat: 增加测试

--- java-learn/src/test/java/Test.java
+++ java-learn/src/test/java/Test.java
@@ -1,22 +1,33 @@
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author fuhailong
  * @Description TODO fhl
  * @Date 2023/8/26 16:20
  **/
+
+@Slf4j
 public class Test {
 
     public static void main(String[] args) {
+        List<TestJson> testJsonList = new ArrayList<>();
+        TestJson testJson1 = new TestJson("A1", "B1");
+        TestJson testJson2 = new TestJson("A3", "B3");
+        TestJson testJson3 = new TestJson("A2", "B2");
+        testJsonList.add(testJson1);
+        testJsonList.add(testJson2);
+        testJsonList.add(testJson3);
+        log.info("msg:{}", JSON.toJSONString(testJsonList));
 
-        String s = "a#b#";
-        String[] strings = s.split("#");
-        String[] strings1 = s.split("#", -1);
-        for (String string : strings) {
-            System.out.println(string);
-        }
-        System.out.println("==================");
-        for (String s1 : strings1) {
-            System.out.println(s1);
-        }
-        System.out.println("=================");
+        String s = "[{\"fieldA\":\"A1\",\"fieldB\":\"B1\"},{\"fieldA\":\"A3\",\"fieldB\":\"B3\"},{\"fieldA\":\"A2\",\"fieldB\":\"B2\"}]";
+        final List<TestJson> testJsons = JSON.parseArray(s, TestJson.class);
+        log.info("res:{}", testJsons);
+
+
     }
+
 }
---
+++ java-learn/src/test/java/TestJson.java
@@ -0,0 +1,19 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.Builder;
+
+/**
+ * @author fuhailong
+ * @Description TODO fhl
+ * @Date 2023/12/1 15:35
+ **/
+
+@AllArgsConstructor
+@Data
+public class TestJson {
+
+    private String fieldA;
+
+    private String fieldB;
+}
+
